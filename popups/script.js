0d7056c0fc4cf5ecd40a3057bfcd1426149c3789
@ZyRa <hung.187pm20486@vanlanguni.vn>
2023-07-20 21:15:52 +0700

    update code

--- popups/script.js
+++ popups/script.js
@@ -1,37 +1,42 @@
 function displayHistory(historyItems) {
     var ul = document.getElementById("history-list");
-    historyItems.slice(0, 5).forEach(function (item) {
-        var historyEntryId = item.id;
-        var url = item.url;
-        var title = item.title;
-        var lastVisitTime = item.lastVisitTime;
-        var visitTimeAgo = getTimeAgo(lastVisitTime);
-
-        var li = document.createElement("li");
-        li.innerHTML =
-            `
-            <div class="item">
-                <span class="title">
-                    <a href="${url}" target="_blank">${title}</a>
-                </span>
-                <span class="time">
-                    At ${new Date(item.lastVisitTime).toLocaleString()} (${visitTimeAgo})
-                </span>
-            </div>
-            <div class="event" data-id="${historyEntryId}">
-                <a class="icon-button-1" title="Access" href="${url}" target="_blank">
-                    <i class="fas fa-link"></i>
-                </a>
-                <button class="icon-button" title="Remove">
-                    <i class="fas fa-trash"></i>
-                </button>
-            </div>`;
-        ul.appendChild(li);
-    });
-
     var qty = document.getElementById("qty");
     qty.innerText = "";
-    qty.innerText = "Quantity: " + historyItems.length;
+    if (historyItems.length > 0) {
+        historyItems.slice(0, 5).forEach(function (item) {
+            var historyEntryId = item.id;
+            var url = item.url;
+            var title = item.title;
+            var lastVisitTime = item.lastVisitTime;
+            var visitTimeAgo = getTimeAgo(lastVisitTime);
+
+            var li = document.createElement("li");
+            li.innerHTML =
+                `
+                <div class="item">
+                    <span class="title">
+                        <a href="${url}" target="_blank">${title}</a>
+                    </span>
+                    <span class="time">
+                        At ${new Date(item.lastVisitTime).toLocaleString()} (${visitTimeAgo})
+                    </span>
+                </div>
+                <div class="event" data-id="${historyEntryId}">
+                    <a class="icon-button-1" title="Access" href="${url}" target="_blank">
+                        <i class="fas fa-link"></i>
+                    </a>
+                    <a class="icon-button" title="Remove">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </div>`;
+            ul.appendChild(li);
+        });
+
+        qty.innerText = "Quantity: " + historyItems.length;
+    }
+    else {
+        generateData();
+    }
 }
 
 function getTimeAgo(timestamp) {
@@ -69,11 +74,7 @@ document.addEventListener("click", function (event) {
 
 function removeHistoryById(historyEntryId) {
     chrome.history.search({ text: "", maxResults: 0 }, function (results) {
-        console.log(results);
-
         var historyEntry = results.find(entry => entry.id === historyEntryId);
-        console.log(historyEntry);
-
         if (historyEntry) {
             // Call the deleteUrl function to remove the history entry
             chrome.history.deleteUrl({ url: historyEntry.url }, function () {
@@ -84,7 +85,7 @@ function removeHistoryById(historyEntryId) {
                     ul.innerHTML = "";
                     getData();
                     toggleElement();
-                }, 2000);
+                }, 500);
             });
         }
     });
@@ -93,11 +94,11 @@ function removeHistoryById(historyEntryId) {
 function toggleElement() {
     var element = document.getElementById("loading");
     if (element.style.display === "none") {
-      element.style.display = "flex";
+        element.style.display = "flex";
     } else {
-      element.style.display = "none";
+        element.style.display = "none";
     }
-  }
+}
 
 function getData() {
     chrome.history.search({ text: '', maxResults: 0 }, function (historyItems) {
@@ -106,3 +107,41 @@ function getData() {
 }
 
 getData();
+
+
+function openTab(evt) {
+    var i, tabContent, tabButton;
+    var tabName = evt.target.getAttribute("data-tab");
+
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabButton = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButton.length; i++) {
+        tabButton[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.target.classList.add("active");
+}
+
+document.getElementById("tab1").style.display = "block";
+document.getElementsByClassName("tab-button")[0].classList.add("active");
+
+var tabButtons = document.getElementsByClassName("tab-button");
+for (var i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener("click", openTab);
+}
+
+function generateData() {
+    var dataList = document.getElementById("content-1");
+
+    // Kiểm tra nếu danh sách không có dữ liệu
+    dataList.innerHTML = "";
+    dataList.innerHTML = `
+        <div class="container">
+        <img src="/icons/bg.png" alt="No Data">
+        </div>`;
+}
